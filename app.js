0bcec2b39f6b7007b650c0ec3abcf850ffba1a60
Gareisignacio <93623301+Gareisignacio@users.noreply.github.com>
2023-12-21 10:53:43 -0300

    Carga de datos

---
+++ app.js
@@ -0,0 +1,73 @@
+const notificationList = [
+   {
+      name: "Mark Webber",
+      action: "reacted to your recent post",
+      postOrGroup: "My first tournament today!",
+      date: "1m ago",
+      perfil_pic: "assets/images/avatar-mark-webber.webp",
+   },
+   {
+      name: "Angela Gray",
+      action: "followed you",
+      postOrGroup: "",
+      date: "5m ago",
+      perfil_pic: "assets/images/avatar-angela-gray.webp",
+   },
+   {
+      name: "Jacob Thompson",
+      action: "has joined your group",
+      postOrGroup: "Chess Club",
+      date: "1 day ago",
+      perfil_pic: "assets/images/avatar-jacob-thompson.webp",
+   },
+   {
+      name: "Rizky Hasanuddin",
+      action: "sent you a private message",
+      postOrGroup: "",
+      date: "5 day ago",
+      perfil_pic: "assets/images/avatar-rizky-hasanuddin.webp",
+   },
+   {
+      name: "Rizky Hasanuddin",
+      action: "sent you a private message",
+      postOrGroup: "",
+      date: "5 day ago",
+      perfil_pic: "assets/images/avatar-rizky-hasanuddin.webp",
+   },
+   {
+      name: "Rizky Hasanuddin",
+      action: "sent you a private message",
+      postOrGroup: "",
+      date: "5 day ago",
+      perfil_pic: "assets/images/avatar-rizky-hasanuddin.webp",
+      message: "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game."
+   },
+   {
+      name: "Kimberly Smith",
+      action: "commented on your picture",
+      postOrGroup: "",
+      date: "1 week ago",
+      perfil_pic: "assets/images/avatar-kimberly-smith.webp",
+      picture:"assets/images/image-chess.webp",
+   },
+   {
+      name: "Nathan Peterson",
+      action: "reacted to your recent post",
+      postOrGroup: "5 end-game strategies to increase your win rate",
+      date: "2 week ago",
+      perfil_pic: "",
+   },
+   {
+      name: "Anna Kim",
+      action: "left the group",
+      postOrGroup: "Chess Club",
+      date: "2 week ago",
+   }
+];
+
+const notifications = document.querySelector(".notifications");
+const markBtn = document.querySelector(".mark-read");
+
+function notificationShow(){
+   
+}
